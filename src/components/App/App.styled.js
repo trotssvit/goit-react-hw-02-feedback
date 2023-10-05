1e9075cba352bf4314290e6d09db191470f5781d
Svitlana Trotsenko <trots.svit@gmail.com>
2023-10-05 22:31:17 +0100

    start1

---
+++ src/components/App/App.styled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-left: auto;
+  margin-right: auto;
+  width: 600px;
+  height: 600px;
+  border-radius: 25px;
+  background-color: white;
+`;
